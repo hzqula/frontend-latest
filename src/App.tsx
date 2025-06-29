e71c7eafb5cc9c0c25792e28f74b8b4d995ddcca
hzqula <ilooqstrasi@gmail.com>
2025-06-29 13:01:28 +0700

    🎓 init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import queryClient from "./lib/queryClient";
+import Register from "./pages/auth/Register";
+import { Toaster } from "sonner";
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            {/* Tambahkan rute lain di sini */}
+          </Routes>
+        </BrowserRouter>
+        <Toaster richColors position="top-right" />
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
