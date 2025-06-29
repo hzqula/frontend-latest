e80967036c961cdd8dcbe056a3d4505b45cdf4c6
hzqula <ilooqstrasi@gmail.com>
2025-06-30 00:51:02 +0700

    🎓 Refactor axios configuration by removing redundant comments for clarity

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5500/api", // Sesuaikan dengan PORT backend
+  baseURL: "http://localhost:5500/api",
   withCredentials: true,
 });
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token"); // Ambil token dari localStorage
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
