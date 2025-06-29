13528d78310b0b2da0bdce241e11603b64714faa
hzqula <ilooqstrasi@gmail.com>
2025-06-30 00:59:00 +0700

    🎓 Refactor component structure in App.tsx for improved readability

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,15 @@ import Login from "./pages/auth/Login";
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <BrowserRouter>
+      <BrowserRouter>
+        <AuthProvider>
           <Routes>
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
-            {/* Tambahkan rute lain di sini */}
           </Routes>
-        </BrowserRouter>
-        <Toaster richColors position="top-right" />
-      </AuthProvider>
+        </AuthProvider>
+      </BrowserRouter>
+      <Toaster richColors position="top-right" />
     </QueryClientProvider>
   );
 };
